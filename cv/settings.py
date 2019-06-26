77ceb7af77a155b88aec0f14162aba51d3d78361
Allison Carrigan <allison.carrigan@yale.edu>
2019-06-26 12:22:52 -0400

    correct static file collection

--- cv/settings.py
+++ cv/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2e7nta$f9+dzzr_^dr%^r&&^@2n-uwd^jo8#fr4agg$^_po!=('
-# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'alliecarrigan.herokuapp.com',
