b4fd3ec91450bffbc89ea0a649849995299c4299
Allison Carrigan <allison.carrigan@yale.edu>
2019-06-26 11:53:04 -0400

    change debug to False

--- cv/settings.py
+++ cv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2e7nta$f9+dzzr_^dr%^r&&^@2n-uwd^jo8#fr4agg$^_po!=('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'alliecarrigan.herokuapp.com',
